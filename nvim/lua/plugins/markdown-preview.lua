02e29f5d35c3ce528e9a60ade376e752e7378688
suzuki-tatsuki <suzuki.tatsuki.20@shizuoka.ac.jp>
2024-05-31 15:19:29 +0900

    [feat] mkdp opens chrome in app mode

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -5,6 +5,9 @@ return{
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
 
+	--init = function() 
+	--end,
+
     config = function()
         local opts = { noremap = true, silent = true }
         local keymap = vim.api.nvim_set_keymap
@@ -13,6 +16,14 @@ return{
         keymap("n", "<C-q>", "<Plug>MarkdownPreviewStop", opts)
         keymap("n", "<C-p>", "<Plug>MarkdownPreviewToggle", opts)
 
+		--vim.g.mkdp_browser = '/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome'
+		vim.cmd([[
+			function OpenMarkdownPreview (url)
+				let cmd = "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --new-window --incognito --app=" . shellescape(a:url) . " &"
+				silent call system(cmd)
+			endfunction
+		]])
+		vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
     end,
 
 }
